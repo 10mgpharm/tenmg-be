ac92868f2b758afd0a4f71fc0973d0ca93ce9426
Kehinde Sholadoye <kehinde.sholadoye@yahoo.com>
2024-09-05 00:23:35 +0100

    signup, signin, verify-email and signout routes

--- routes/api.php
+++ routes/api.php
@@ -1,17 +1,37 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Auth\AuthenticatedController;
+use App\Http\Controllers\Auth\SignupUserController;
+use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => ['cors', 'json.response']], function () {
 
-    // public routes
+    Route::prefix('v1')->group(function () {
 
-    // protected routes
-    Route::middleware('auth:api')->group(function () {
+        // public routes
+        Route::prefix('auth')->group(function () {
 
-        Route::get('/user', function (Request $request) {
-            return $request->user();
+            Route::middleware('guest')->group(function () {
+                Route::post('/signup', [SignupUserController::class, 'store'])
+                    ->name('signup');
+
+                Route::post('/signin', [AuthenticatedController::class, 'store'])
+                    ->name('singin');
+            });
+
+            Route::middleware(['auth:api', 'scope:temp'])->group(function () {
+                Route::post('/verify-email', VerifyEmailController::class)
+                    ->name('verification.verify');
+            });
+
+
+            // protected routes
+            Route::middleware(['auth:api', 'scope:full'])->group(function () {
+
+                Route::post('/signout', [AuthenticatedController::class, 'destroy'])
+                    ->name('signout');
+            });
         });
     });
 });
