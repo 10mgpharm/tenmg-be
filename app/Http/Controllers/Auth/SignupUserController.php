a3ae544f38eaeba3793a6626d9f3e67dc2af6da6
Kehinde Sholadoye <kehinde.sholadoye@yahoo.com>
2024-09-05 00:20:29 +0100

    Create SignupUserController.php

---
+++ app/Http/Controllers/Auth/SignupUserController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Mail\Mailed;
+use App\Enums\Enums\MailType;
+use App\Helpers\UtilityHelper;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Response;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Mail;
+use App\Http\Requests\Auth\SignupUserRequest;
+use App\Http\Resources\UserResource;
+
+class SignupUserController extends Controller
+{
+
+    /**
+     * Handle an incoming signup request.
+     *
+     * @param SignupUserRequest $request
+     * @return JsonResponse
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function store(SignupUserRequest $request): JsonResponse
+    {
+        try {
+            
+            return DB::transaction(function () use ($request) {
+
+                $user = $request->register();
+
+                $otp = $user->otps()->create([
+                    'code' => UtilityHelper::generateOtp(),
+                    'type' => 'SIGNUP_EMAIL_VERIFICATION',
+                ]);
+        
+                $tokenResult = $user->createToken('Temporary Access Token', ['temp']);
+                
+                Mail::to($user->email)->send(new Mailed($user, MailType::REGISTRATION_VERIFICATION, ['otp' => $otp]));
+            
+                return (new UserResource($user))->additional([
+                    'temporal_access_token' => $tokenResult->accessToken,
+                    'token_type' => 'Bearer',
+                    'expires_at' => $tokenResult->token->expires_at,
+                    'message' => 'Sign up successful. Please verify your email using the OTP sent.',
+                ])->response()
+                ->setStatusCode(Response::HTTP_CREATED);
+            });
+
+        } catch (\Throwable $th) {
+            return response()->json( [
+                'message' => $th->getMessage(),
+                'file' => $th->getFile(),
+                'line' => $th->getLine(),
+                'trace' => $th->getTraceAsString(),
+                'previous' => $th->getPrevious(),
+                'code' => $th->getCode(),
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
+    }
+
+}
