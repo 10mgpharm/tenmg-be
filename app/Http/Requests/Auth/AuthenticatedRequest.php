0d2d12494f4f1a35c6a08ad01c726138ceb54800
Kehinde Sholadoye <kehinde.sholadoye@yahoo.com>
2024-09-05 00:21:08 +0100

    Create AuthenticatedRequest.php

---
+++ app/Http/Requests/Auth/AuthenticatedRequest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Requests\Auth;
+
+use Illuminate\Auth\Events\Lockout;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+
+class AuthenticatedRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'email' => ['required', 'string', 'email'],
+            'password' => ['required', 'string'],
+        ];
+    }
+
+    /**
+     * Attempt to authenticate the request's credentials.
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     * @return bool
+     */
+    public function authenticate(): bool
+    {
+        if (!Auth::attempt($this->only('email', 'password'), $this->boolean('remember'))) {
+            return false;
+        }
+
+        return true;
+    }
+
+}
