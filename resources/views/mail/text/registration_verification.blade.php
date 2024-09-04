2dbe46b780e60d40bc0b524c80e4eaa8278ac591
Kehinde Sholadoye <kehinde.sholadoye@yahoo.com>
2024-09-05 00:20:06 +0100

    mail templates

---
+++ resources/views/mail/text/registration_verification.blade.php
@@ -0,0 +1,15 @@
+# Welcome to {{ config('app.name') }}!
+
+Thank you for registering with us. To complete your registration and verify your email address, please use the One-Time
+Password (OTP) provided below:
+
+**Your OTP:** {{ $otp->code }}
+
+This OTP is valid for 15 minutes, so please use it promptly. If you did not register for an account, you can ignore this email.
+
+If you have any questions or need assistance, feel free to reach out to our support team.
+
+Thank you for joining {{ config('app.name') }}!
+
+Best Regards,
+The {{ config('app.name') }} Team
