e9772a592e3fe953db637038efb1b890b7b19839
Kehinde Sholadoye <kehinde.sholadoye@yahoo.com>
2024-09-11 23:09:22 +0100

    needed migration files

---
+++ app/Models/EcommercePayout.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class EcommercePayout extends Model
+{
+    use HasFactory;
+}
