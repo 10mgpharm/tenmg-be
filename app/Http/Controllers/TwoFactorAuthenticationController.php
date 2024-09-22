fd9de72696206ada8a0148592f7e21e8befb2379
Kehinde Sholadoye <kehinde.sholadoye@yahoo.com>
2024-09-22 19:48:16 +0100

    Create TwoFactorAuthenticationController.php

---
+++ app/Http/Controllers/TwoFactorAuthenticationController.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Enums\OtpType;
+use App\Http\Resources\UserResource;
+use App\Http\Requests\ResetTwoFactorRequest;
+use App\Http\Requests\SetupTwoFactorRequest;
+use App\Http\Requests\VerifyTwoFactorRequest;
+use App\Services\OtpService;
+use Illuminate\Support\Facades\DB;
+
+class TwoFactorAuthenticationController extends Controller
+{
+    private $google2fa;
+
+    public function __construct()
+    {
+        $this->google2fa = app('pragmarx.google2fa');
+    }
+
+    /**
+     * Setup multi-factor authentication (MFA) for the user.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function setup(SetupTwoFactorRequest $request)
+    {
+        $user = $request->user();
+        $two_factor_secret = $this->google2fa->generateSecretKey();
+
+        DB::transaction(fn () =>
+            $user->forceFill([
+                'two_factor_secret' => encrypt($two_factor_secret),
+                'use_two_factor' => true,
+            ])->save()
+        );
+
+        $qrcode_xml = $this->google2fa->getQRCodeInline(
+            config('app.name'),
+            $user->email,
+            $two_factor_secret
+        );
+
+        $data = [
+            'qrcode_svg' => $qrcode_xml,
+            'two_factor_secret' => $two_factor_secret,
+        ];
+
+        if (config('app.env') == 'local') {
+            return response($qrcode_xml, 200)
+                ->header('Content-Type', 'image/svg+xml');
+        }
+
+        return $this->returnJsonResponse(
+            message: "Two-factor authentication setup completed.",
+            data: $data
+        );
+    }
+
+    /**
+     * Verify the provided multi-factor authentication token.
+     *
+     * @param  \App\Http\Requests\VerifyTwoFactorRequest  $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function verify(VerifyTwoFactorRequest $request)
+    {
+        try {
+            $user = $request->user();
+
+            DB::transaction(function () use ($user) {
+                $user->forceFill(['use_two_factor' => true])->save();
+            });
+
+            $tokenResult = $user->createToken('Full Access Token', ['full']);
+
+            return $this->returnJsonResponse(
+                data: (new UserResource($user->refresh()))->additional([
+                    'tokenResult' => $tokenResult
+                ]),
+                message: 'Two-factor authentication verified successfully.'
+            );
+        } catch (\Throwable $th) {
+            return $this->handleErrorResponse($th);
+        }
+    }
+
+    /**
+     * Reset multi-factor authentication for the user.
+     *
+     * @param  \App\Http\Requests\ResetTwoFactorRequest  $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function reset(ResetTwoFactorRequest $request)
+    {
+        try {
+            $user = $request->user();
+            $otp = (new OtpService)->validate(OtpType::RESET_MULTI_FACTOR_AUTHENTICATION, $request->input('otp'));
+
+            DB::transaction(function () use ($user, $otp) {
+                $user->forceFill([
+                    'two_factor_secret' => null,
+                    'use_two_factor' => false,
+                ])->save();
+
+                $otp->delete();
+            });
+
+            return $this->returnJsonResponse(
+                message: 'Multi-factor authentication has been reset. You will need to set it up again.',
+                data: new UserResource($user->refresh())
+            );
+        } catch (\Throwable $th) {
+            return $this->handleErrorResponse($th);
+        }
+    }
+}
