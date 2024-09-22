381c92da58c3540c0b0a080a495919c9857c7d3f
Kehinde Sholadoye <kehinde.sholadoye@yahoo.com>
2024-09-22 19:53:33 +0100

    Update Business.php

--- app/Models/Business.php
+++ app/Models/Business.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Business extends Model
 {
@@ -12,4 +13,21 @@ class Business extends Model
     protected $table = 'businesses';
 
     protected $guarded = [];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @param array<string, string>
+     */
+    protected $casts = [
+        'expiry_date' => 'date',
+    ];
+
+    /**
+     * Get the user that owns the otp.
+     */
+    public function owner(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'owner_id');
+    }
 }
