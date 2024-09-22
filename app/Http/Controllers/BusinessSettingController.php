f5d6187ff09b2aaff6cce28948c5a79e6f30edc2
Kehinde Sholadoye <kehinde.sholadoye@yahoo.com>
2024-09-22 19:45:23 +0100

    Create BusinessSettingController.php

---
+++ app/Http/Controllers/BusinessSettingController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\BusinessSettingAccountSetupRequest;
+use App\Services\AttachmentService;
+use App\Http\Resources\BusinessResource;
+use App\Http\Requests\ShowBusinessSettingRequest;
+use App\Http\Requests\BusinessSettingPersonalInformationRequest;
+use App\Http\Resources\UserResource;
+
+class BusinessSettingController extends Controller
+{
+    public function __construct(private AttachmentService $attachmentService,) {}
+
+    /**
+     * Display the business details associated with the authenticated user.
+     *
+     * @param \App\Http\Requests\ShowBusinessSettingRequest $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function show(ShowBusinessSettingRequest $request)
+    {
+        $user = $request->user()->load('ownerBusinessType.owner');
+
+        return $this->returnJsonResponse(
+            message: 'Business details successfully retrieved.',
+            data: (new BusinessResource($user->ownerBusinessType))
+        );
+    }
+
+    /**
+     * Update the business personal information details associated with the authenticated user.
+     *
+     * @param \App\Http\Requests\BusinessSettingPersonalInformationRequest $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function personalInformation(BusinessSettingPersonalInformationRequest $request)
+    {
+        $validated = $request->validated();
+
+        $data = array_filter(array_intersect_key(
+            $validated,
+            array_flip(['name', 'contact_person', 'contact_phone', 'contact_email', 'address'])
+        ));  // since fillable isn't used.
+
+        $request->user()->ownerBusinessType()->update($data);
+
+        return $this->returnJsonResponse(
+            message: 'Business personal information details successfully updated.',
+            data: (new BusinessResource($request->user()->ownerBusinessType->refresh()))
+        );
+    }
+
+    /**
+     * Update the business account license number, expiry date and cac doc 
+     * details associated with the authenticated user.
+     *
+     * @param \App\Http\Requests\BusinessSettingAccountSetupRequest $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function accountSetup(BusinessSettingAccountSetupRequest $request)
+    {
+        $validated = $request->validated();
+
+        $data = array_filter(array_intersect_key(
+            $validated,
+            array_flip(['license_number', 'expiry_date'])
+        ));  // since fillable isn't used.
+
+        $request->user()->ownerBusinessType()->update($data);
+
+        return $this->returnJsonResponse(
+            message: 'Business account setup details successfully updated.',
+            data: (new BusinessResource($request->user()->ownerBusinessType->refresh()))
+        );
+    }
+}
