22e7b6a7f088382946a0968b1952096394daa929
Kehinde Sholadoye <kehinde.sholadoye@yahoo.com>
2024-10-01 15:33:25 +0100

    Create GuestAcceptInviteRequest.php

---
+++ app/Http/Requests/GuestAcceptInviteRequest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+use Illuminate\Validation\Rules;
+
+
+class GuestAcceptInviteRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+
+    /**
+     * Prepare the data for validation.
+     */
+    protected function prepareForValidation(): void
+    {
+        $this->merge([
+            'invite_token' => $this->query('inviteToken'),
+            'invite_id' => $this->query('inviteId'),
+        ]);
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'invite_id' => [
+                'required',
+                Rule::exists('invites', 'id')->where(
+                    fn($query) => $query
+                        ->where('invite_token', $this->query('inviteToken'))
+                        ->where('status', 'INVITED')
+                ),
+            ],
+            'invite_token' => ['required', 'string'],
+            'password' => ['required', Rules\Password::default()],
+            'passwordConfirmation' => ['required', 'same:password'],
+            'termsAndConditions' => ['required', 'accepted'],
+        ];
+    }
+
+
+    /**
+     * Custom error messages for validation failures.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'invite_id.exists' => 'The invitation is invalid, expired, or has already been used.',
+        ];
+    }
+}
