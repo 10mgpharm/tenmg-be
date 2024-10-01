e9ba0175affa027e9b9291def562143b9363b805
Kehinde Sholadoye <kehinde.sholadoye@yahoo.com>
2024-10-01 15:32:11 +0100

    Create BusinessUserResource.php

---
+++ app/Http/Resources/BusinessUserResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class BusinessUserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'role' => $this->role->name,
+            'active' => $this->active,
+            'email' => $this->user->email,
+            'name' => $this->user->name
+        ];
+        return parent::toArray($request);
+    }
+}
