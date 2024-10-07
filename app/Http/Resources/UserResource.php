3b6760de5a62c6468cc8af35247258668cd89d28
Kehinde Sholadoye <kehinde.sholadoye@yahoo.com>
2024-10-07 22:48:14 +0100

    Update UserResource.php

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -19,10 +19,9 @@ class UserResource extends JsonResource
             'name' => $this->name,
             'email' => $this->email,
             'active' => (bool) $this->active == 1,
-            'useTwoFactor' => (
-                $this->use_two_factor && $this->two_factor_secret ? 'ACTIVE'
-                : (!$this->use_two_factor && $this->two_factor_secret ? 'INACTIVE' : 'NOT_SETUP' )
-            ),
+            'useTwoFactor' => $this->two_factor_secret ?
+                ($this->use_two_factor ? 'ACTIVE' : 'INACTIVE') :
+                'NOT_SETUP',
             'avatar' => $this->avatar,
             'emailVerifiedAt' => $this->email_verified_at,
             'owner' => (bool) ($this->ownerBusinessType?->type),
