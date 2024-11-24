288763016480ca4a8e230f316f1975d9f140162b
Kehinde Sholadoye <kehinde.sholadoye@yahoo.com>
2024-11-24 15:41:31 +0100

    Create 2024_11_23_064119_add_column_to_ecommerce_products_table.php

---
+++ database/migrations/2024_11_23_064119_add_column_to_ecommerce_products_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use App\Enums\StatusEnum;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('ecommerce_products', function (Blueprint $table) {
+            // Add 'status' column if it doesn't exist
+            if (!Schema::hasColumn('ecommerce_products', 'status')) {
+                $table->enum('status', array_map(fn($status) => $status->value, StatusEnum::cases()))
+                    ->default(StatusEnum::PENDING->value)
+                    ->nullable();
+            } else {
+                // Update 'status' column if it exists
+                $table->enum('status', array_map(fn($status) => $status->value, StatusEnum::cases()))
+                    ->default(StatusEnum::PENDING->value)
+                    ->nullable()
+                    ->change();
+            }
+        
+            // Add 'status_comment' column if it doesn't exist
+            if (!Schema::hasColumn('ecommerce_products', 'status_comment')) {
+                $table->text('status_comment')->nullable();
+            }
+        });
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('ecommerce_products', function (Blueprint $table) {
+            //
+        });
+    }
+};
