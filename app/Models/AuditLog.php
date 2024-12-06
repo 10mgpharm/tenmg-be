e44a980932259b7e64b661db053444c811357e99
Kehinde Sholadoye <kehinde.sholadoye@yahoo.com>
2024-12-06 08:15:51 +0100

    Update AuditLog.php

--- app/Models/AuditLog.php
+++ app/Models/AuditLog.php
@@ -10,6 +10,14 @@ class AuditLog extends Model
     use HasFactory;
 
     /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'audit_logs';
+
+
+    /**
      * The attributes that are mass assignable.
      *
      * @var array<int, string>
