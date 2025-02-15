2ecf0d9c960090668ce47d21605be2617305b12c
Ahmed Olanrewaju <dev@consode.com>
2025-02-15 15:58:19 +0100

    fix: file name

--- app/Models/creditLendersPreference.php
+++ app/Models/creditLendersPreference.php
@@ -5,6 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property int $id
+ * @property int $lender_id
+ * @property array $loan_tenure
+ * @property int $loan_interest
+ * @property array $credit_score_category
+ * @property bool $auto_accept
+ */
 class CreditLendersPreference extends Model
 {
     use HasFactory;
@@ -14,12 +22,12 @@ class CreditLendersPreference extends Model
         'loan_tenure',
         'loan_interest',
         'credit_score_category',
-        'auto_accept'
+        'auto_accept',
     ];
 
     protected $casts = [
         'loan_tenure' => 'array',
         'credit_score_category' => 'array',
-        'auto_accept' => 'boolean'
+        'auto_accept' => 'boolean',
     ];
 }
