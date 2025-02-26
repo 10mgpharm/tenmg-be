858fe2557a8358183f8e511f0624375c17471a00
Kehinde Sholadoye <kehinde.sholadoye@yahoo.com>
2025-02-26 08:33:27 +0100

    fix: add actor name and email

--- app/Http/Resources/Admin/AuditLogResource.php
+++ app/Http/Resources/Admin/AuditLogResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources\Admin;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -20,6 +21,7 @@ class AuditLogResource extends JsonResource
         'event' => $this->event,
         'createAt' => $this->created_at ? $this->created_at->format('M d, y h:i A') : null,
         'properties' => $this->properties->except(['actor_business_id', 'target_business_id'])->toArray(),
+        'actor' =>$this->properties->get('actor_business_id') ? User::find($this->properties->get('actor_business_id'))?->only(['name', 'email']) : null,
     ];
     }
 }
