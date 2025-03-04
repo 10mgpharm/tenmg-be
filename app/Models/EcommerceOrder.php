5689ffb55caad1383ef4514c7124ab8f408dc0dc
uhweka13 <uhweka@gmail.com>
2025-03-04 07:14:56 +0100

    fix:get lenders query and get order customer query fixed

--- app/Models/EcommerceOrder.php
+++ app/Models/EcommerceOrder.php
@@ -48,7 +48,7 @@ class EcommerceOrder extends Model
      */
     public function customer()
     {
-        return $this->belongsTo(Customer::class);
+        return $this->belongsTo(User::class, 'customer_id', 'id');
     }
 
     /**
