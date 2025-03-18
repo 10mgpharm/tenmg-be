32ad7c36e61b001ca20fc7bc0c7f0c6b33dc6e6f
uhweka13 <uhweka@gmail.com>
2025-03-18 19:44:16 +0100

    feat:loan canceled after 5 days automatically done

--- routes/console.php
+++ routes/console.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Console\Commands\CancelUnapprovedLoans;
 use App\Console\Commands\MarkExpiredDiscountStatus;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
@@ -10,3 +11,4 @@ Artisan::command('inspire', function () {
 })->purpose('Display an inspiring quote')->hourly();
 
 Schedule::command(MarkExpiredDiscountStatus::class)->dailyAt('00:00');
+Schedule::command(CancelUnapprovedLoans::class)->dailyAt('00:00');
